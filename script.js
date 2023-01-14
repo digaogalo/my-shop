2076c7482428edf87364eb0a2f2a6e29e3a3d396
digaogalo <digaolimac@gmail.com>
2023-01-14 17:20:04 -0300

    myshop

---
+++ script.js
@@ -0,0 +1,21 @@
+
+class Produto {
+
+    constructor() {
+        this.id = 0 
+        this.nomeProduto = ""
+        this.valor = 0
+        }
+
+        
+        salvar(){
+            alert("Salvo")
+    }
+    
+        cancelar() {
+        alert('Cancelado')
+    }
+
+}
+
+var produto = new Produto()
