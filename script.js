d59454db37f6d1f1c349df4b698d858f641e9441
digaogalo <digaolimac@gmail.com>
2023-01-14 18:45:48 -0300

    alteracao-js

--- script.js
+++ script.js
@@ -2,16 +2,54 @@
 class Produto {
 
     constructor() {
-        this.id = 0 
+        this.id = 1 
+        this.arrayProdutos = []
         }
 
     
         salvar(){
-            this.lerDados()
+          let produto =  this.lerDados()
+
+          if (this.validaCampos(produto)){
+            this.adicionar(produto)
+          }
+
+          console.log(this.arrayProdutos)
         }
+
+        adicionar(produto) {
+            this.arrayProdutos.push(produto)
+            this.id++
+        }
+
         lerDados(){
-            alert('ler dados')
+           let produto = {}
+
+           produto.id = this.id
+           produto.nomeProduto = document.getElementById('produto').value
+           produto.preco = document.getElementById('preco').value
+
+           return produto
         }
+
+        validaCampos(produto) {
+            let msg = ""
+
+            if (produto.nomeProduto == "") {
+                msg += "- Informe o nome do produto\n"
+            }    
+                if (produto.preco == "") {
+                    msg +=  "- Informe o preço do produto\n"   
+            }
+
+            if (msg != "") {
+                alert(msg)
+                return false
+            }
+
+            return true
+        }
+
         cancelar() { 
 
         }
