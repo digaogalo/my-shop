baca29e973289e9eb098570dde5e3820a3345f74
digaogalo <digaolimac@gmail.com>
2023-01-15 18:12:55 -0300

    new changes

--- script.js
+++ script.js
@@ -14,7 +14,38 @@ class Produto {
             this.adicionar(produto)
           }
 
-          console.log(this.arrayProdutos)
+          this.listatabela()
+          this.cancelar()
+        }
+
+        listatabela() {
+            let tbody = document.getElementById('tbody')
+            tbody.innerText = ''
+
+            for(let i = 0; i < this.arrayProdutos.length; i++) {
+                let tr = tbody.insertRow()
+
+                let td_id = tr.insertCell()
+                let td_produto = tr.insertCell()
+                let td_valor = tr.insertCell()
+                let td_acoes = tr.insertCell()
+
+                td_id.innerText = this.arrayProdutos[i].id
+                td_produto.innerText = this.arrayProdutos[i].nomeProduto
+                td_valor.innerText = this.arrayProdutos[i].preco
+
+                td_id.classList.add('center')
+
+                let imgEdit = document.createElement('img')
+                imgEdit.src = 'img/edit.png'
+
+                let imgDelete = document.createElement('img')
+                imgDelete.src = 'img/bin.png'
+                imgDelete.setAttribute('onclick', 'produto.deletar('+ this.arrayProdutos[i].id +')')
+
+                td_acoes.appendChild(imgEdit)
+                td_acoes.appendChild(imgDelete)
+            }
         }
 
         adicionar(produto) {
@@ -51,7 +82,12 @@ class Produto {
         }
 
         cancelar() { 
+            document.getElementById('produto').value = ''
+            document.getElementById('preco').value = ''
+        }
 
+        deletar(id) {
+    alert(`delete o ID ${id}`)
         }
 
 }
