04d863aeec45accd63da7ea00081a667c5d252dc
digaogalo <digaolimac@gmail.com>
2023-01-16 18:36:50 -0300

    js alterações

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ class Produto {
     constructor() {
         this.id = 1 
         this.arrayProdutos = []
+        this.editId = null
         }
 
     
@@ -11,7 +12,11 @@ class Produto {
           let produto =  this.lerDados()
 
           if (this.validaCampos(produto)){
-            this.adicionar(produto)
+            if(this.editId == null) {
+                this.adicionar(produto)
+            } else {
+                this.atualizar(this.editId, produto)
+            }
           }
 
           this.listatabela()
@@ -38,6 +43,7 @@ class Produto {
 
                 let imgEdit = document.createElement('img')
                 imgEdit.src = 'img/edit.png'
+                imgEdit.setAttribute('onclick', 'produto.preparaEditar('+ JSON.stringify(this.arrayProdutos[i]) +')')
 
                 let imgDelete = document.createElement('img')
                 imgDelete.src = 'img/bin.png'
@@ -49,8 +55,27 @@ class Produto {
         }
 
         adicionar(produto) {
+            produto.preco = parseFloat(produto.preco)
             this.arrayProdutos.push(produto)
             this.id++
+        }
+
+        atualizar(id, produto) {
+            for(let i = 0; i < this.arrayProdutos.length; i++) {
+                if(this.arrayProdutos[i].id == id) {
+                    this.arrayProdutos[i].nomeProduto = produto.nomeProduto
+                    this.arrayProdutos[i].preco = produto.preco
+                }
+            }
+        }
+
+        preparaEditar(dados) {
+            this.editId = dados.id
+
+            document.getElementById('produto').value = dados.nomeProduto
+            document.getElementById('preco').value = dados.preco
+
+            document.getElementById('btn1').innerText = "Atualizar"
         }
 
         lerDados(){
@@ -84,10 +109,27 @@ class Produto {
         cancelar() { 
             document.getElementById('produto').value = ''
             document.getElementById('preco').value = ''
+
+            document.getElementById('btn1').innerText = 'Salvar'
+            this.editId = null
         }
 
         deletar(id) {
-    alert(`delete o ID ${id}`)
+            if (confirm(`Tem certeza que quer deletar ID ${id} ?`)) {
+
+                let tbody = document.getElementById('tbody')
+
+                for(let i = 0; i < this.arrayProdutos.length; i++) {
+    
+                    if (this.arrayProdutos[i].id == id) {
+                        this.arrayProdutos.splice(i, 1)
+                        tbody.deleteRow(i)
+                    }
+                }
+    
+                console.log(this.arrayProdutos)
+            }
+
         }
 
 }
